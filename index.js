619ff28cd78a75d890cfb8b9435496adaaaae174
MaxineCarraway <misscmax@hotmail.com>
2023-11-10 13:42:31 +0000

    Amends, Amends All the amends!

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(5000, () => console.log(`App running on port ${port}`));
+app.listen(port, () => console.log(`App running on port ${port}`));
